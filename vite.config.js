7e2e28f8370b0d0d9854c948c3c915109e97c865
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2022-10-25 14:14:49 +0200

    change JS output filename

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: 'conditional-logic-repeater.js',
+			},
+		},
+	},
 	plugins: [
 		svelte({
 			compilerOptions: {
